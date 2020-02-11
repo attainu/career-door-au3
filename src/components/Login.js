0efa05613a48a554e3412153d465a17e67e88fe8
vineet patil <vinit1168@gmail.com>
2020-02-11 09:52:36 +0530

    changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,29 @@
 import React from 'react';
-
+import useSignUpForm from './customhook';
 const Login=()=>{
+    const login = () =>{
+        alert(`${inputs.email}`)
+    }
+    const {inputs, handleInputChange, handleSubmit} = useSignUpForm({email:'',password:''}, login);
     return(
 <div>
-<form>
+<form onSubmit={handleSubmit}>
                 <h3>Sign In</h3>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" className="form-control" placeholder="Enter email" name="email" onChange={handleInputChange} value={inputs.email}/>
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" className="form-control" placeholder="Enter password" name="password" onChange={handleInputChange} value={inputs.password}/>
                 </div>
 
                 
                 
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <button type="submit" className="btn btn-primary btn-block" >Submit</button>
                 
                 
             </form>
