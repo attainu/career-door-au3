6731276ca241d06ac517706fea024e5dce83ea45
karthik-rao-au3 <karthikb786@gmail.com>
2020-03-09 16:53:38 +0530

    minor chnages

--- app.js
+++ app.js
@@ -49,6 +49,13 @@ app.use("/profile", profileRouter);
 app.use("/user", userRoute);
 app.use("/review", reviewRouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 // aceess control origin
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
